f2b562dc10565614c0f42eece0ca31876711898c
ljdongs <ljdongs@126.com>
2019-05-21 22:03:17 +0800

    main.js数字LED 修改

--- main/main.js
+++ main/main.js
@@ -1,4 +1,66 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    let strs = num.toString().split("")
+    let str = ''
+    if (strs.length > 0){
+        for(let i = 0; i < strs.length;i++){
+           str = str + choseNum(strs[i])
+        }
+    }
+    return str;
 };
+function choseNum(num) {
+   switch (num) {
+   case "0":
+          return ("._. \n" + 
+                  "|.| \n" +
+                  "|_| \n")
+          break;
+   case "1":
+          return ("... \n" + 
+                  "..| \n" +
+                  "..| \n")
+          break;
+   case "2":
+          return ("._. \n" + 
+                  "._| \n" +
+                  "|_. \n")
+          break;
+   case "3":
+          return ("._. \n" + 
+                  "._| \n" +
+                  "._| \n")
+          break;
+   case "4":
+          return ("... \n" + 
+                  "|_| \n" +
+                  "..| \n")
+          break;
+   case "5":
+          return ("._. \n" + 
+                  "|_. \n" +
+                  "._| \n")
+          break;
+   case "6":
+          return ("._. \n" + 
+                  "|_. \n" +
+                  "|_| \n")
+          break;
+   case "7":
+          return ("._. \n" + 
+                  "..| \n" +
+                  "..| \n")
+          break;
+   case "8":
+          return ("._. \n" + 
+                  "|_| \n" +
+                  "|_| \n")
+          break;
+   case "9":
+          return ("._. \n" + 
+                  "|_| \n" +
+                  "..| \n")
+          break;
+   default:
+       break;
+   }
+}
